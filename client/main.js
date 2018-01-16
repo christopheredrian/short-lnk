0dec8b89862a2d4148c9d9a41c44f307a0876bb0
Christopher Espiritu <christopheredrian@gmail.com>
2018-01-16 09:33:59 +0800

    Initial components

---
+++ client/main.js
@@ -0,0 +1,32 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, BrowserRouter, Switch } from 'react-router-dom';
+
+import Signup from '../imports/ui/Signup';
+import Link from '../imports/ui/Link';
+import Login from '../imports/ui/Login';
+import NotFound from '../imports/ui/NotFound';
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/signup" component={Signup}/>
+        <Route exact path="/link" component={Link}/>
+        <Route component={NotFound}/>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+/**
+ * When meteor is ready
+ */
+Meteor.startup(function(){
+  /**
+   * React router
+   */
+  ReactDOM.render(routes, document.getElementById("app"));
+});
---
+++ imports/ui/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class Signup extends React.Component{
+  render(){
+    return (
+      <h1>Signup component</h1>
+    );
+  }
+}
